e6779ba8da85ad68e6dece3d78ff06f3a1e00ef7
Constantin Mihai <mitza_tkd@yahoo.com>
2021-01-09 18:36:12 +0200

    Add project files.

---
+++ Data/Constantin_Mihai_lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Constantin_Mihai_lab8.Models;
+
+namespace Constantin_Mihai_lab8.Data
+{
+    public class Constantin_Mihai_lab8Context : DbContext
+    {
+        public Constantin_Mihai_lab8Context (DbContextOptions<Constantin_Mihai_lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Constantin_Mihai_lab8.Models.Book> Book { get; set; }
+    }
+}
